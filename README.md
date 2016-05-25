28f91ca50b856235f43728261493e93ba594cd93
Tony Mutai <tonimut7@gmail.com>
2016-05-25 16:29:58 +0300

    add client files

---
+++ README.md
@@ -0,0 +1,100 @@
+# text-kenya
+
+Client wrapper for Text Kenya Web Service.
+
+## Install.
+
+```
+$ npm install text-kenya
+```
+
+## Examples.
+
+```javascript
+  var textKenya = require('text-kenya');
+  var data = { u: 'username', h: 'token', to: '0700112233', msg: 'To SMS and Infinity' };
+
+  textKenya.sendSMS(data, function (err, response) {
+    if(err) return console.log(err);
+
+    // operate on response here
+    console.log(response);
+  });
+```
+## API.
+
+**textKenya.sendSMS(data#Object, cb(err, response)#Function)**
+
+Send SMS to a single or multiple numbers
+
+`data` params:
+  - mandatory: `to`, `msg`, `u`, `h`
+  - optional: `type`, `unicode`, `from`, `footer`, `nofooter`, `format`
+
+**textKenya.outgoingSMS(data#Object, cb(err, response)#Function)**
+
+Outgoing SMS and delivery status
+
+`data` params:
+  - mandatory: `u`, `h`
+  - optional: `queue`, `src`, `dst`, `dt`, `smslog_id`, `c`, `last`, `format`
+
+**textKenya.incomingSMS(data#Object, cb(err, response)#Function)**
+
+List Incoming SMS
+
+`data` params:
+  - mandatory: `u`, `h`
+  - optional: `queue`, `src`, `dst`, `dt`, `smslog_id`, `c`, `last`, `format`
+
+**textKenya.showInbox(data#Object, cb(err, response)#Function)**
+
+List SMS on user's inbox.
+
+`data` params:
+  - mandatory: `u`, `h`
+  - optional: `queue`, `src`, `dst`, `dt`, `smslog_id`, `c`, `last`, `format`
+
+**textKenya.getUserCredit(data#Object, cb(err, response)#Function)**
+
+Get user's credit information.
+
+`data` params:
+  - mandatory: `u`, `h`
+  - optional: `format`
+
+**textKenya.getToken(data#Object, cb(err, response)#Function)**
+
+Get user's webservice token. This can be used as a login mechanism.
+
+`data` params:
+  - mandatory: `u`, `p`
+  - optional: `format`
+
+**textKenya.setToken(data#Object, cb(err, response)#Function)**
+
+Set user's webservice token. This can be used as a password change mechanism.
+
+`data` params:
+  - mandatory: `u`, `h`
+  - optional: `format`
+
+**textKenya.getContactList(data#Object, cb(err, response)#Function)**
+
+Get contact list by code or name
+
+`data` params:
+  - mandatory: `u`, `h`, `kwd`
+  - optional: `format`, `c`
+
+**textKenya.serverInfo(data#Object, cb(err, response)#Function)**
+
+Query server for useful information such as user's data, user's credit, last smslog_id for inbox,incoming and outgoing SMS
+
+`data` params:
+  - mandatory: `u`, `h`
+  - optional: `format`
+
+## LICENSE.
+
+MIT.
---
+++ index.js
@@ -0,0 +1,152 @@
+/**
+ * Load Module Dependencies
+ */
+var request   = require('request');
+var merge     = require('merge');
+
+var WEB_SERVICE_URL = 'http://textkenya.com/text/index.php';
+
+/**
+ * Send SMS
+ *
+ * Params:
+ *   - mandatory: to, msg, u, h
+ *   - optional: type, unicode, from, footer, nofooter, format
+ */
+exports.sendSMS = function sendSMS(data, cb) {
+  data = merge(data, { op: 'pv' });
+  return sendRequest(data, cb);
+};
+
+/**
+ * Outgoing SMS and Delivery status
+ *
+ * Params:
+ *    - mandatory: u, h
+ *    - optional: queue, src, dst, dt, smslog_id, c, last, format
+ */
+exports.outgoingSMS = function outgoingSMS(data, cb) {
+  data = merge(data, { op: 'ds' });
+  return sendRequest(data, cb);
+};
+
+/**
+ * List Incoming SMS
+ *
+ * Params:
+ *    - mandatory: u, h
+ *    - optional: queue, src, dst, dt, smslog_id, c, last, format
+ */
+exports.incomingSMS = function incomingSMS(data, cb) {
+  data = merge(data, { op: 'in' });
+  return sendRequest(data, cb);
+};
+
+/**
+ * List SMS on user's inbox
+ *
+ * Params:
+ *    - mandatory: u, h
+ *    - optional: queue, src, dst, dt, smslog_id, c, last, format
+ */
+exports.showInbox = function showInbox(data, cb) {
+  data = merge(data, { op: 'ix' });
+  return sendRequest(data, cb);
+};
+
+/**
+ * Get User's Credit information
+ *
+ * Params:
+ *    - mandatory: u, h
+ *    - optional: format
+ */
+exports.getUserCredit = function getUserCredit(data, cb) {
+  data = merge(data, { op: 'cr' });
+  return sendRequest(data, cb);
+};
+
+/**
+ * Get User's webservice token.
+ * This can be used as a login mechanism
+ *
+ * Params:
+ *    - mandatory: u, p
+ *    - optional: format
+ */
+exports.getToken = function getToken(data, cb) {
+  data = merge(data, { op: 'get_token' });
+  return sendRequest(data, cb);
+};
+
+/**
+ * Set User's webservice token.
+ * This can be used as a password change mechanism.
+ *
+ * Params:
+ *    - mandatory: u, h
+ *    - optional: format
+ */
+exports.setToken = function setToken(data, cb) {
+  data = merge(data, { op: 'set_token' });
+  return sendRequest(data, cb);
+};
+
+/**
+ * Get Contact List by name or code
+ *
+ * Params:
+ *    - mandatory: u, h, kwd
+ *    - optional: format, c
+ */
+exports.getContactList = function getContactList(data, cb) {
+  data = merge(data, { op: 'get_contact_group' });
+  return sendRequest(data, cb);
+};
+
+/**
+ * Query Server for useful info.
+ *
+ * Params:
+ *    - mandatory: u, h
+ *    - optional: format
+ */
+exports.serverInfo = function serverInfo(data, cb) {
+  data = merge(data, { op: 'query' });
+  return sendRequest(data, cb);
+};
+
+/**
+ * Wrapper for request handler
+ *
+ * @private
+ */
+function sendRequest(payload, cb) {
+  var info;
+  var qsParams = { app: 'ws', u: '',  h: '' };
+
+  var qs = merge(qsParams, payload);
+  info = {
+    uri: WEB_SERVICE_URL,
+    qs: qs
+  };
+
+  request(info, function (err, res, body) {
+    if(err) return cb(err);
+
+    try {
+      body = JSON.parse(body);
+    } catch(ex) {
+      return cb(ex);
+    }
+
+    if(body.error_string) {
+      err = new Error(body.error_string);
+      err.code = body.error;
+
+      return cb(err);
+    }
+
+    cb(null, body);
+  });
+}
